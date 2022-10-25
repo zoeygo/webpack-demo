99bcf0a71b271586d1ba7e0603b86453ec1aa7b4
沈霞 <xia.shen@going-link.com>
2022-10-25 14:58:22 +0800

    feat(demo1024): css变量使用和部分类名不改变的写法

--- src/routes/Test1/index.tsx
+++ src/routes/Test1/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import styles from './index.less';
@@ -6,8 +6,14 @@ import styles from './index.less';
 const Test1 = () => {
     const navigate = useNavigate();
 
+    useEffect(() => {
+        const getDom = document.getElementById('test1');
+        // 只能获取这种不会变的样式名，其他编译后与style文件中不一致
+        console.log('getDom test1===', getDom);
+    }, []);
+
     return (
-        <div className={styles.content} id={styles.test1}>
+        <div className={styles.content} id="test1">
             <div onClick={() => { navigate('/test2'); }}>
                 点击跳转到Test2
             </div>
