cd4c8f57cc10a0b52581fe7c37cd09d1d14845a0
沈霞 <xia.shen@going-link.com>
2022-11-09 16:36:28 +0800

    feat(demo1024): div模拟textarea支持插入标签

--- src/routes/QuickReply/index.tsx
+++ src/routes/QuickReply/index.tsx
@@ -3,7 +3,6 @@
  * div支持插入标签等
  */
 import { Tag } from 'antd';
-import { CloseOutlined } from '@ant-design/icons';
 import React, { memo, useRef, useCallback } from 'react';
 
 import styles from './index.less';
@@ -16,24 +15,27 @@ interface TagProps {
 
 const QuickReply: React.FC = () => {
     const inputRef: any = useRef(null);
-    const tagText: TagProps[] = [{ content: '1' }, { content: '2' }];
+    const tagText: TagProps[] = [{ content: '标签1' }, { content: '标签2' }];
 
     // 添加tag标签到输入框
     const addTag = useCallback((value: string) => {
         const node = document.createElement("span");
+        node.style.border = "1px solid #e0e0e0";
+        node.style.cursor = "pointer";
+        node.style.marginLeft = "1rem";
         node.innerText = value;
-        const cancelNode = document.createElement("span");
-        cancelNode.innerHTML = `${<CloseOutlined />}`;
+        const cancelNode = document.createElement("Tag");
+        cancelNode.innerText = "✕";
         cancelNode.onclick = (event: any) => {
             const target = event.target;
-            if (inputRef) {
-                inputRef.removeChild(target.parentElement);
+            if (inputRef.current) {
+                inputRef.current.removeChild(target.parentElement);
             }
         };
         node.append(cancelNode);
         if (position === '') {
             // 没有div光标，则在div内容末尾插入
-            inputRef.appendChild(node);
+            inputRef.current.appendChild(node);
         } else if (typeof position === 'object') {
             position.insertNode(node);
         }
@@ -48,21 +50,23 @@ const QuickReply: React.FC = () => {
         <div
             className={styles['quick-reply']}
         >
-            <div className='drag-hr'>
-                <hr />
-            </div>
             <div className='quick-reply-content'>
                 <div
                     className='quick-reply-content-input'
                     contentEditable="true"
                     ref={inputRef}
                     onBlur={handleBlur}
-                >
-                    输入区
-                </div>
+                />
                 <div className='quick-reply-content-tag'>
                     {
-                        tagText.map(item => <Tag onClick={() => addTag(item.content)}>{item.content}</Tag>)
+                        tagText.map((item, index) =>
+                            <Tag
+                                onClick={() => addTag(item.content)}
+                                key={`tag_${index}`}
+                            >
+                                {item.content}
+                            </Tag>
+                        )
                     }
                 </div>
             </div>
