f5de0d06751f6d93614ed936518fcc8d83781a1f
沈霞 <xia.shen@going-link.com>
2026-02-28 14:35:54 +0800

    add: utils类型校验

--- src/routes/utils.ts
+++ src/routes/utils.ts
@@ -136,4 +136,14 @@ function isObject(x) {
   return Object.prototype.toString.call(x) === '[object Object]'
 }
 
-export { handleDownload, getRandom, shallowEqual, getBase64FromImageUrl, isObject }
+// 获取数据类型
+function getType(data) {
+  // 获取到 "[object Type]"，其中 Type 是 Null、Undefined、Array、Function、Error、Boolean、Number、String、Date、RegExp 等。
+  const originType = Object.prototype.toString.call(data)
+  // 可以直接截取第8位和倒数第一位，这样就获得了 Null、Undefined、Array、Function、Error、Boolean、Number、String、Date、RegExp 等
+  const type = originType.slice(8, -1)
+  // 再转小写，得到 null、undefined、array、function 等
+  return type.toLowerCase()
+}
+
+export { handleDownload, getRandom, shallowEqual, getBase64FromImageUrl, isObject, getType }
